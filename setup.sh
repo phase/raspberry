8a35aa0d09d0469e46421659439e6b8d62c9f2e7
phase <ph4sesaber@gmail.com>
2015-05-07 14:49:35 -0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# C, C++, Java, Scratch, Python and Ruby all come installed by default on the Raspberry Pi.
+
+# Install GCC (Not needed on `Jessie`, but needed on `Wheezy`
+sudo apt-get install gcc-4.9 g++-4.9
+
+# Install PHP
+sudo apt-get install php5 libapache2-mod-php5 php5-mcrypt
+
+# Install Perl
+sudo apt-get install perl
